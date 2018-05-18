47cea27668afe02d8bbdd3f7fdb2f5ebf0e9cd81
ConJov <39412235+ConJov@users.noreply.github.com>
2018-05-18 23:06:32 +0800

    Add files via upload

---
+++ Task 1 - NZJ.py
@@ -0,0 +1,10 @@
+"""Task 1 - N Z J"""
+
+Sessions_Attended = {'sessions' : '1011,2344,3222,44322,555,6332,721,8789,99,1011,1124,1245,137,1499'}
+
+n = Sessions_Attended['sessions'] 
+n = n.split(",")
+
+print("I have attended : ", len(n), "sessions")
+
+
---
+++ Task 2- NZJ.py
@@ -0,0 +1,13 @@
+"""Task 2 - N Z J"""
+
+import random
+rd = random.randint(1,10)
+while True:
+    number = int(input("Guess the number: "));
+    if number == rd:
+        print("Correct!");   
+        break
+    elif number!= rd:
+        print("Wrong, try again!"); 
+    
+
